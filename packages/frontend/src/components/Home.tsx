1918ab05b8cdcf739f115acc1721e1a28373d84d
Rafael Solorzano <rafael.solorzano@gov.bc.ca>
2023-02-05 21:14:43 -0800

    updated home to fetch data and show it on overview

--- packages/frontend/src/components/Home.tsx
+++ packages/frontend/src/components/Home.tsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react"
 import Container from "react-bootstrap/Container"
+import GetAllBoats from "../api/GetAllBoats"
+import { BoatState } from "../interfaces/BoatState"
 import Overview from "./Overview"
 
-const Home = () => (
-    <Container fluid className="p-3">
-        <Container fluid className="p-5 mb-4 bg-light rounded-3">
-            <h1 className="header">EcoCatch Tours</h1>
-            <p>Welcome to EcoCatch Tours, current boat conditions:</p>
-            <Overview horizontal={false} />
+const Home = () => {
+    const [boats, setBoats] = useState<BoatState>({
+        docked: [],
+        outbound: [],
+        inbound: [],
+        maintenance: []
+    })
+    useEffect(() => {
+        GetAllBoats(setBoats)
+    }, [])
+    return (
+        <Container fluid className="p-3">
+            <Container fluid className="p-5 mb-4 bg-light rounded-3">
+                <h1 className="header">EcoCatch Tours</h1>
+                <p>Welcome to EcoCatch Tours, current boat conditions:</p>
+                <Overview horizontal={false} boats={boats} />
+            </Container>
         </Container>
-    </Container>
-)
+    )
+}
 
 export default Home
--- packages/frontend/src/components/Overview.tsx
+++ packages/frontend/src/components/Overview.tsx
@@ -1,14 +1,15 @@
 import Badge from "react-bootstrap/Badge"
 import ListGroup from "react-bootstrap/ListGroup"
+import { BoatState } from "../interfaces/BoatState"
 
-const Overview = ({ horizontal = false }: { horizontal: boolean | string }) => (
+const Overview = ({ horizontal = false, boats }: { horizontal: boolean | string; boats: BoatState }) => (
     <ListGroup as="ul" horizontal={horizontal}>
         <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
             <div className="ms-2 me-auto">
                 <div className="fw-bold">Docked</div>
             </div>
             <Badge bg="secondary" pill>
-                14
+                {boats.docked.length}
             </Badge>
         </ListGroup.Item>
         <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
@@ -16,7 +17,7 @@ const Overview = ({ horizontal = false }: { horizontal: boolean | string }) => (
                 <div className="fw-bold">Outbound to Sea</div>
             </div>
             <Badge bg="info" pill>
-                14
+                {boats.outbound.length}
             </Badge>
         </ListGroup.Item>
         <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
@@ -24,7 +25,7 @@ const Overview = ({ horizontal = false }: { horizontal: boolean | string }) => (
                 <div className="fw-bold">Inbound to Harbor</div>
             </div>
             <Badge bg="primary" pill>
-                14
+                {boats.inbound.length}
             </Badge>
         </ListGroup.Item>
         <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
@@ -32,7 +33,7 @@ const Overview = ({ horizontal = false }: { horizontal: boolean | string }) => (
                 <div className="fw-bold">Maintenance</div>
             </div>
             <Badge bg="warning" pill>
-                14
+                {boats.maintenance.length}
             </Badge>
         </ListGroup.Item>
     </ListGroup>
